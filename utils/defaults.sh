f09694e18017f381f37591c88436e1c5ee515be6
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-01-26 14:44:25 +0100

    1. Fixed path typos.

--- utils/defaults.sh
+++ utils/defaults.sh
@@ -7,10 +7,10 @@ declare CCTL_COMPILE_TARGET=${CSPR_COMPILE_TARGET:-release}
 _CCTL_PATH_TO_WORKING_DIR="$( cd "$( dirname "${CCTL[0]}" )" && pwd )"
 
 # Set path -> casper-client.
-declare CCTL_PATH_TO_CASPER_CLIENT=${CSPR_COMPILE_TARGET:-$_CCTL_PATH_TO_WORKING_DIR/casper-client-rs}
+declare CCTL_PATH_TO_CASPER_CLIENT=${CSPR_PATH_TO_CASPER_CLIENT:-$_CCTL_PATH_TO_WORKING_DIR/casper-client-rs}
 
 # Set path -> casper-node.
-declare CCTL_PATH_TO_CASPER_NODE=${CSPR_COMPILE_TARGET:-$_CCTL_PATH_TO_WORKING_DIR/casper-node}
+declare CCTL_PATH_TO_CASPER_NODE=${CSPR_PATH_TO_CASPER_NODE:-$_CCTL_PATH_TO_WORKING_DIR/casper-node}
 
 # Set path -> casper-node launcher.
 declare CCTL_PATH_TO_CASPER_NODE_LAUNCHER=${CSPR_PATH_TO_NODE_LAUNCHER:-$_CCTL_PATH_TO_WORKING_DIR/casper-node-launcher}
