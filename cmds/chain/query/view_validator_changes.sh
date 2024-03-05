648543e1a828deab2ec9f3348faa530c40e34414
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-03-05 11:41:19 +0100

    1. New cmd: cctl-chain-view-validator-changes.

---
+++ cmds/chain/query/view_validator_changes.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+function _help() {
+    echo "
+    COMMAND
+    ----------------------------------------------------------------
+    cctl-chain-view-validator-changes
+
+    DESCRIPTION
+    ----------------------------------------------------------------
+    Displays validator change set.
+
+    ARGS
+    ----------------------------------------------------------------
+    node        Identifier of a node. Optional.
+
+    DEFAULTS
+    ----------------------------------------------------------------
+    node        1
+    "
+}
+
+function _main()
+{
+    local NODE_ID=${1}
+
+    echo "$(get_node_address_rpc "$NODE_ID")"
+
+    $(get_path_to_client) get-validator-changes \
+        --node-address "$(get_node_address_rpc "$NODE_ID")" \
+        | jq '.result.changes'
+}
+
+# ----------------------------------------------------------------
+# ENTRY POINT
+# ----------------------------------------------------------------
+
+source "$CCTL"/utils/main.sh
+
+unset _HELP
+unset _NODE_ID
+
+for ARGUMENT in "$@"
+do
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
+    case "$KEY" in
+        help) _HELP="show" ;;
+        node) _NODE_ID=${VALUE} ;;
+        *)
+    esac
+done
+
+if [ "${_HELP:-""}" = "show" ]; then
+    _help
+else
+    _main "${_NODE_ID:-"1"}"
+fi
