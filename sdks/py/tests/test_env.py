ffd2aa1ed4550de652a006d664a858fc8a8f0040
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-03-22 12:14:59 +0100

    1. removed SDK sub folder.

--- sdks/py/tests/test_env.py
+++
@@ -1,5 +0,0 @@
-import pycctl
-
-
-def test_that_evars_are_declared():
-    assert pycctl.env.get_evar(pycctl.env.EVarType.CCTL) is not None
--- sdks/py/tests/test_interface.py
+++
@@ -1,5 +0,0 @@
-import pycctl
-
-
-def test_version_of_library():
-    assert pycctl.__version__ == "0.1.0"
