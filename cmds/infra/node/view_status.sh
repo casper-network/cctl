445b8b8c0d27a802143fad921cbe8a2723fda57a
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-06-05 17:56:29 +0200

    1. Moving status api invocations from json-rpc.

--- cmds/infra/node/view_status.sh
+++ cmds/infra/node/view_status.sh
@@ -28,7 +28,6 @@ function _main()
         for NODE_ID in $(seq 1 "$CCTL_COUNT_OF_NODES")
         do
             if [ $(get_is_node_up "$NODE_ID") = true ]; then
-                echo "------------------------------------------------------------------------------------------------------------------------------------"
                 _display_status "$NODE_ID"
             fi
         done
@@ -45,23 +44,24 @@ function _main()
 function _display_status()
 {
     local NODE_ID=${1}
-    local NODE_ADDRESS_CURL=$(get_address_of_sidecar_main_server_for_curl "$NODE_ID")
-    local NODE_API_RESPONSE
-    
+
+    local ENDPOINT="$(get_address_of_node_rest_server "$NODE_ID")"/status
+
     NODE_API_RESPONSE=$(
         curl $CCTL_CURL_ARGS_FOR_NODE_RELATED_QUERIES --header 'Content-Type: application/json' \
-            --request POST "$NODE_ADDRESS_CURL" \
-            --data-raw '{
-                "id": 1,
-                "jsonrpc": "2.0",
-                "method": "info_get_status"
-            }' | jq '.result'
+            --location \
+            --request GET "$ENDPOINT" \
+            | jq '.'
     )
 
     if [ -z "$NODE_API_RESPONSE" ]; then
+        log "------------------------------------------------------------------------------------------------------------------------------------"
         log "node #$NODE_ID :: status: N/A"
+        log "------------------------------------------------------------------------------------------------------------------------------------"
     else
+        log "------------------------------------------------------------------------------------------------------------------------------------"
         log "node #$NODE_ID :: status:"
+        log "------------------------------------------------------------------------------------------------------------------------------------"
         echo "$NODE_API_RESPONSE" | jq '.'
     fi
 }
