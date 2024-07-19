5be08e8eade757d00f6325cf812e5461878ab753
Carl Norburn <carl@stormeye.co.uk>
2024-07-19 15:34:50 +0100

    docker script changes

--- docker/clean.sh
+++ docker/clean.sh
@@ -13,8 +13,9 @@ rm -r casper-node-launcher/target/release/.fingerprint
 rm -r casper-client-rs/target/release/build
 rm -r casper-client-rs/target/release/deps
 rm -r casper-client-rs/target/release/.fingerprint
+rm -r casper-sidecar/target/release/build
+rm -r casper-sidecar/target/release/deps
+rm -r casper-sidecar/target/release/.fingerprint
 rm -r casper-node/.git
 rm -r casper-client-rs/.git
 rm -r casper-node-launcher/.git
-rm -r casper-node/utils/nctl
-rm -r casper-node/utils/nctl-metrics
