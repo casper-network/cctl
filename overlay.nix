407b24d689006e79328c23c68c17a2180a69f2ed
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-09-09 12:49:50 +0200

    flake: use casper-client-rs feature 2.0 package

--- overlay.nix
+++ overlay.nix
@@ -1,3 +1,6 @@
 final: prev: {
-  cctl = final.callPackage ./package.nix { casper-node = final.casper-node_2; };
+  cctl = final.callPackage ./package.nix {
+    casper-node = final.casper-node_2;
+    casper-client-rs = final.casper-client-rs_2;
+  };
 }
